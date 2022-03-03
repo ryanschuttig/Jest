28ebcbccc6983cc7708c82668b84855479523409
Ryan Schuttig <ryan.schuttig@capco.com>
2022-03-03 16:33:35 -0500

    jest test

--- functions/functions.test.js
+++ functions/functions.test.js
@@ -1,6 +1,36 @@
 const { test, expect } = require('@jest/globals');
 const functions = require('./functions');
 
+// toBe
 test('adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
+});
+
+// not
+test('adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+// toBeNull
+test('should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+
+// toBeFalsy
+test('should be falsy', () => {
+    expect(functions.checkValue(undefined)).toBeFalsy();
+});
+
+test('should be John Doe object', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'John',
+        lastName: 'Doe'
+    });
 });
