91edd657be8c01c5a9ed8c3bb0b78928a5d0619b
Ryan Schuttig <ryan.schuttig@capco.com>
2022-03-03 13:15:41 -0500

    basics

--- sum/sum.test.js
+++ sum/sum.test.js
@@ -2,5 +2,7 @@ const { test, expect } = require('@jest/globals');
 const sum = require('./sum');
 
 test('properly adds two numbers', () => {
-    expect(sum(1, 2)).toBe(3)
+    expect(
+        sum(1, 2)
+        ).toBe(3)
 });
